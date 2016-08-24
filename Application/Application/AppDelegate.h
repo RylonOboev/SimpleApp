54915083ce0195858bba22184e372721de2e4ffc
Rulon Oboev <expert_po_texnike@mail.ru>
2016-08-24 15:12:02 +0300

    Added project.

---
+++ Application/Application/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Application
+//
+//  Created by Rylon Oboev on 8/24/16.
+//  Copyright © 2016 Rylon Oboev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Application/Application/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Application
+//
+//  Created by Rylon Oboev on 8/24/16.
+//  Copyright © 2016 Rylon Oboev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
